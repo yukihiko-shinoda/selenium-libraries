8ee3f2a2da91d444aa9dfb99ab4bcb9e4f9aed8c
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-04-29 15:26:18 +0900

    Add  function to safely get text

--- __init__.py
+++ __init__.py
@@ -1,1 +1,24 @@
 """The package about Selenium library."""
+
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def get_text(element: WebElement) -> str:
+    """Gets Selenium element text.
+
+    - Answer: python - Selenium returns empty string instead of the actual data - Stack Overflow
+      https://stackoverflow.com/a/68737909/12721873
+    Args:
+        element: Selenium web element
+    Returns: Text of the element
+    """
+    text = element.text
+    if text:
+        return text
+    inner_text = element.get_attribute("innerText")
+    if inner_text:
+        return inner_text
+    text_content = element.get_attribute("textContent")
+    if text_content:
+        return text_content
+    return ""
